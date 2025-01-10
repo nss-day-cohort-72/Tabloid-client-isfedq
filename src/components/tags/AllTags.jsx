5aed4d5a46dd6332ee753f2e08a3348565687ab8
gunnarebeling <gunnarebeling@icloud.com>
2025-01-10 11:12:54 -0600

    create new tagh

--- src/components/tags/AllTags.jsx
+++ src/components/tags/AllTags.jsx
@@ -1,28 +1,56 @@
-import { useEffect, useState } from "react";
-import { Card, Col, Container, Row } from "reactstrap";
-import { getAllTags } from "../../managers/TagManager";
+import { useContext, useEffect, useState } from "react";
+import { Button, Card, Col, Container, Input, InputGroup, Row } from "reactstrap";
+import { getAllTags, postTag } from "../../managers/TagManager";
+import { UserContext } from "../ApplicationViews";
 
 export const AllTags = () => {
     const [tags, setTags] = useState([]);
     const [newTag, setNewTag] = useState("");
+    const { loggedInUser } = useContext(UserContext);
 
     useEffect(() => {
         getAllTags().then(setTags);
     }, []);
+
+    const handleTagChange = (e) => {
+        setNewTag(e.target.value);
+    }
+
+    const handleSaveTag = () => {
+        const postObj = { name: newTag };
+        postTag(postObj)
+            .then(() => {
+                getAllTags()
+                    .then(setTags);
+            });
+    }
     return (
-        <Container className="mt-3">
-        <h1 className="text-center mb-4">All Tags</h1>
-        <Row className="justify-content-center">
-            {tags.map(tag => (
-                <Col key={tag.id} xs="12" sm="10" md="8" lg="6" className="mb-3">
-                    <Card className="shadow-sm w-100">
-                        <Container className="d-flex ps-0 justify-content-between ">
-                            <h4 className="m-2 me-4">{tag.name}</h4>
-                        </Container>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
-    </Container>
+        <Container className="mt-3 d-flex flex-column align-items-center">
+            <h1 className="mb-5">All Tags</h1>
+            <ul className="list-unstyled">
+                {tags.map(tag => (
+                    <li key={tag.id}>
+                        <Card className="shadow-sm mb-3">
+                            <Container className="d-flex ps-0 justify-content-between">
+                                <h4 className="m-2 me-4">{tag.name}</h4>
+                            </Container>
+                        </Card>
+                    </li>
+                ))}
+            </ul>
+            {loggedInUser.roles?.some(r => r === "Admin") && (
+                <div className="d-flex">
+                    <InputGroup>
+                        <Input
+                            type="text"
+                            value={newTag}
+                            onChange={handleTagChange}
+                            placeholder="New tag name"
+                        />
+                        <Button color="primary" onClick={handleSaveTag} >Add</Button>
+                    </InputGroup>
+                </div>
+            )}
+        </Container>
     )
 }
--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -3,3 +3,13 @@ export const getAllTags = () => {
     return fetch(`${api_url}`)
         .then(res => res.json());
 }
+
+export const postTag = (tag) => {
+    return fetch(`${api_url}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tag)
+    });
+}
